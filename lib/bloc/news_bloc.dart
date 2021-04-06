7e51bcfe48170203e93edd3ddd290f56a726cc05
AdeelZahid <m.adeel2144@gmail.com>
2021-04-06 13:01:58 +0500

    api manager working properly

--- lib/bloc/news_bloc.dart
+++ lib/bloc/news_bloc.dart
@@ -1,8 +1,6 @@
-import 'package:http/http.dart' as http;
 import 'dart:async';
-import 'dart:convert';
+import 'package:bloc_beginner/services/api_manager.dart';
 import 'package:bloc_beginner/model/newsInfo.dart';
-import 'package:bloc_beginner/constants/strings.dart';
 
 enum NewsAction { fetch, delete }
 
@@ -18,8 +16,9 @@ class NewsBloc {
   NewsBloc() {
     _eventStream.listen((event) async {
       if (event == NewsAction.fetch) {
+        API_Manager apiManager = API_Manager();
         try {
-          var news = await getNews();
+          var news = await apiManager.getNews();
 
           if (news != null) {
             _newsSink.add(news.articles);
@@ -34,27 +33,6 @@ class NewsBloc {
         return 0;
       }
     });
-  }
-
-  Future<NewsModel> getNews() async {
-    var client = http.Client();
-    var newsModel;
-
-    try {
-      // http.Response response = await client.get(
-      //     'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=256cc8a1c2c04a75a5fe8743cebfdc13');
-      http.Response response = await client.get(Strings.news_url);
-      print('RESPONSE: ${response.body}');
-      if (response.statusCode == 200) {
-        var jsonString = response.body;
-        var jsonMap = json.decode(jsonString);
-
-        newsModel = NewsModel.fromJson(jsonMap);
-      }
-    } catch (Exception) {
-      return newsModel;
-    }
-    return newsModel;
   }
 
   void newsBlocDispose() {
--- lib/services/api_manager.dart
+++ lib/services/api_manager.dart
@@ -1,26 +1,28 @@
-// import 'dart:convert';
-// import 'package:bloc_beginner/constants/strings.dart';
-// import 'package:bloc_beginner/model/newsInfo.dart';
-// import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:bloc_beginner/constants/strings.dart';
+import 'package:bloc_beginner/model/newsInfo.dart';
+import 'package:http/http.dart' as http;
 
-// // ignore: camel_case_types
-// class API_Manager {
-//   Future<NewsModel> getNews() async {
-//     var client = http.Client();
-//     var newsModel;
+// ignore: camel_case_types
+class API_Manager {
+  Future<NewsModel> getNews() async {
+    var client = http.Client();
+    var newsModel;
 
-//     try {
-//       var response = await client.get(Strings.news_url);
-//       print('RESPONSE: $response');
-//       if (response.statusCode == 200) {
-//         var jsonString = response.body;
-//         var jsonMap = json.decode(jsonString);
+    try {
+      // http.Response response = await client.get(
+      //     'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=256cc8a1c2c04a75a5fe8743cebfdc13');
+      http.Response response = await client.get(Strings.news_url);
+      print('RESPONSE: ${response.body}');
+      if (response.statusCode == 200) {
+        var jsonString = response.body;
+        var jsonMap = json.decode(jsonString);
 
-//         newsModel = NewsModel.fromJson(jsonMap);
-//       }
-//     } catch (Exception) {
-//       return newsModel;
-//     }
-//     return newsModel;
-//   }
-// }
+        newsModel = NewsModel.fromJson(jsonMap);
+      }
+    } catch (Exception) {
+      return newsModel;
+    }
+    return newsModel;
+  }
+}
